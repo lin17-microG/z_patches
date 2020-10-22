d99fc57a4465f42079c711be3c9424d983a2abf8
MSe1969 <mse1969@posteo.de>
2020-10-22 22:53:07 +0200

    Initial z_patches creation

---
+++ patches_apply.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+THISDIR=$PWD
+
+cd ..
+TOPDIR=$PWD
+
+cd vendor/lineage
+echo "Patching $PWD (build signing method)"
+patch -p1 < $THISDIR/patch_002_vendor-lineage.patch
+echo "-"
+cd $TOPDIR
+
+cd $THISDIR
+
+
+
+
+
