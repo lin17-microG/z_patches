56ea24f882cf916dc676a78d4fa143dc32879cc5
mse1969 <mse1969@posteo.de>
2021-02-08 23:10:10 +0100

    OTA: Apply lineage.updater.uri patch
    Change-Id: I7b37dcacb704bd6bf432d179c81ce547d8c6db5e

--- patches_apply.sh
+++ patches_apply.sh
@@ -8,6 +8,7 @@ TOPDIR=$PWD
 cd vendor/lineage
 echo "Patching $PWD (build signing method)"
 patch -p1 < $THISDIR/patch_002_vendor-lineage.patch
+patch -p1 < $THISDIR/patch_003_vendor-lineage.patch
 echo "-"
 cd $TOPDIR
 
